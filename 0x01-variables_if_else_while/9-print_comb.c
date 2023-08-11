2f071e75b60879f82eda0046bfb902dde35640ae
timileyin42 <akanmuibro@gmail.com>
2023-08-11 13:06:26 -0700

    printing-if-else-while-statement

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,13 @@ int main(void)
 int a;
 for (a = 0; a < 10; a++)
 {
-if (a == 9)
 putchar(a + '0');
-else
+if (a != 9)
 {
-putchar(a + '0');
 putchar(',');
 putchar(' ');
 }
-
 }
+putchar('\n');
 return (0);
 }
