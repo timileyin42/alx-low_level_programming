ceb2d60d56575b3fc3c5cd8d8230a3c63f99b9bf
timileyin42 <akanmuibro@gmail.com>
2023-08-11 11:48:27 -0700

    putchar0_commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - functions to be executed
+*
+* Return: (0)
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+putchar(a + '0');
+putchar('\n');
+return (0);
+}
