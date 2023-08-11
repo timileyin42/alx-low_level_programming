6ce40b278572e7f07a1bf0844db007779234dc9b
timileyin42 <akanmuibro@gmail.com>
2023-08-11 11:59:49 -0700

    reverse-loop-commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - functions to be executed
+*
+* Return: (0)
+*/
+
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
