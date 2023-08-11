9de7d43dc2dfb760aa222a81b8ba8ad3afd3012f
timileyin42 <akanmuibro@gmail.com>
2023-08-11 11:34:07 -0700

    base10-loop-commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - functions to be executed
+*
+* Return: - (0)
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+printf("\n");
+return (0);
+}
