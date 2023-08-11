756ea6b3c2d9aa3d78fe444e8274a8bdbde560ae
timileyin42 <akanmuibro@gmail.com>
2023-08-11 10:45:11 -0700

    for_loop-commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - functions to be executed
+*
+* Return: always(0)
+*/
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
+
