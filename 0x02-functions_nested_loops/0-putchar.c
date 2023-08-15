172bcb3483f7d9f1ee93fadcab7740e1d20f93b2
timileyin42 <akanmuibro@gmail.com>
2023-08-15 11:24:09 -0700

    printing-putchar-commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - main functions to be printed
+* main - printing of putchar
 *
 * Return: (0)
 */
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+_putchar('\n');
 
 return (0);
 }
