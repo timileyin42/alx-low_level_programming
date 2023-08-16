a4371b837f854e6e57dad1bcea0142a7ccec7067
timileyin42 <akanmuibro@gmail.com>
2023-08-16 11:04:06 -0700

    functions-else-loop_commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 int sum = 0;
