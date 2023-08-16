ffd6826d40c09f6c37f9bd7126180684ee1a661f
timileyin42 <akanmuibro@gmail.com>
2023-08-16 05:38:18 -0700

    time_table-printig_commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,32 @@
  */
 void times_table(void)
 {
-int x, y, product;
+int x, y, a, b, c;
 
 for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
-product = x * y;
-
-if (y != 0)
+a = x * y;
+if (a > 9)
 {
-_putchar(',');
-_putchar(' ');
-
-if (product < 10)
-_putchar(' ');
-}
-if (product < 10)
-{
-_putchar(' ');
-_putchar(product + '0');
+b = a % 10;
+c = (a - b) / 10;
+_putchar(c + '0');
+_putchar(b + '0');
 }
 else
 {
-_putchar((product / 10) + '0');
-_putchar((product % 10) + '0');
+if (y != 0)
+{
+_putchar(' ');
+}
+_putchar(a + '0');
+}
+if (y < 9)
+{
+_putchar(',');
+_putchar(' ');
 }
 }
 _putchar('\n');
