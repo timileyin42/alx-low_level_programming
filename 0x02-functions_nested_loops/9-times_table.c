eedadb774c350e09296ffae6682f85b5cf9f632a
timileyin42 <akanmuibro@gmail.com>
2023-08-16 03:41:32 -0700

    time_table-printig_commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+int x, y, product;
+
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+product = x * y;
+
+if (y != 0)
+{
+_putchar(',');
+_putchar(' ');
+
+if (product < 10)
+_putchar(' ');
+}
+if (product < 10)
+{
+_putchar(' ');
+_putchar(product + '0');
+}
+else
+{
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 #endif
