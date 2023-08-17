e8c48be79009ba88eb005727f31d7ff17683aa06
timileyin42 <akanmuibro@gmail.com>
2023-08-17 03:43:25 -0700

    printing-num-from-0-14_commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 to 14 (repeated 10 times).
+ *
+ * Return: (0)
+ */
+void more_numbers(void)
+{
+int x, y;
+
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y >= 10)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
+}
+
