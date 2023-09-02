15c0a7bde9e3b8b72bd5cc69788f2f43e0413177
timileyin42 <akanmuibro@gmail.com>
2023-09-02 13:25:36 -0700

    printing-of-all-arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,11 +10,13 @@
 
 int main(int argc, char *argv[])
 {
+	int x = 0;
+
 	(void) argc;
 	(void) argv;
 
 	/* loop to iterate through all arguement and print */
-	for (int x = 0; x < argc; x++)
+	for (x = 0; x < argc; x++)
 	{
 		printf("%s\n", argv[x]);
 	}
