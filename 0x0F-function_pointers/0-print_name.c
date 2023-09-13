e2e910ed082328a05d08b9060959e569cb499189
timileyin42 <akanmuibro@gmail.com>
2023-09-13 09:11:19 -0700

    printing-of-name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,14 +4,13 @@
 * print_name - printing of a name.
 * @name: name to be printed.
 * @f: function pointers to the printing function.
-*
 * Return: (void)
 */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-	return;
+		return;
 	f(name);
 }
 
