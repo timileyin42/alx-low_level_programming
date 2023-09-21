1baeb9704029346c28eb6e12fb0f8aed6488e926
timileyin42 <akanmuibro@gmail.com>
2023-09-21 06:10:59 -0700

    printing

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a singly linked list and counts nodes
+ *
+ * @h:  A pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	/* initializing the count to 0 */
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+
+		/* looping through the other node */
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+Singly Linked List Project
+This is a simple implementation of a singly linked list data structure in [c programming language]. A singly linked list is a fundamental data structure that consists of a sequence of nodes, each containing a data element and a reference to the next node in the list.
